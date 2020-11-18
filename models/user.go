6759ce13c20bea613f54dae4853b470b1c22c280
dinuka-rp <drpiyadigama@gmail.com>
2020-11-18 14:49:41 +0530

    Create methods in user type & update .gitignore

--- models/user.go
+++ models/user.go
@@ -10,3 +10,16 @@ var (
 	users  []*User // (collection of) slice of users with pointers to User objects
 	nextID = 1
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u) // &u grabs a reference to the user object that was provided
+	// (because pointers to User objects are only stored in the users slice)
+
+	return u, nil
+}
