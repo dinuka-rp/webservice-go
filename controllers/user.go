1b53203a273c6a9dbf6bebea85c715ef0a0a72f1
dinuka-rp <drpiyadigama@gmail.com>
2020-11-18 15:40:56 +0530

    Add constrcutor with regex validation to controller method

--- controllers/user.go
+++ controllers/user.go
@@ -1,10 +1,23 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type userController struct{}
+type userController struct{
+	userIDPattern *regexp.Regexp
+
+}
 
 // method (basically another function)
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the User Controller!"))
+}
+
+// constructor
+func newUserController() *userController{
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`),
+	}
 }
